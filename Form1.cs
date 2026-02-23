d612860f29cbf0458a6ebd04b7a747559c3ece17
AlfonsoMakajumba <juliusz129@gmail.com>
2026-02-23 04:28:27 +0100

    Barebones file choosing button implementation

--- Form1.cs
+++ Form1.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,5 +18,34 @@ namespace mp4ClipCutter
         {
             InitializeComponent();
         }
+        private void chooseFile_Click(object sender, EventArgs e)
+        {
+            Debug.WriteLine("Dialog opened");
+            //add an option to change the default path of fileDialog
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                Debug.WriteLine(fileDialog.FileName);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //fileDialog already initialized in Form1.Designer.cs
+            fileDialog.Filter = "mp4 files|*.mp4";
+            Debug.WriteLine("Program launched");
+        }
+
+        private void resolutionChange_CheckedChanged(object sender, EventArgs e)
+        {
+            if (resolutionChange.Checked == true)
+            {
+                resDropDown.Visible = true;
+
+            }
+        }
+    }
+    public partial class Config
+    {
+
     }
 }
